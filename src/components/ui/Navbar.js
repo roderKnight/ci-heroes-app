3c569129ea6dd05877c6d13461329e34440e97f9
roderknight <dealarcmarcel99@gmail.com>
2021-11-20 18:44:18 -0400

    implementing useNavigate hook

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,11 +1,14 @@
 import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import { Link, NavLink, useNavigate } from 'react-router-dom'
 
 export const Navbar = () => {
 
+    const navigate = useNavigate();
+
     const handleLogout = () => {
-        // TODO por hacer
-        console.log('logout');
+        navigate('login', {
+            replace: true
+        });
     }
 
     return (
