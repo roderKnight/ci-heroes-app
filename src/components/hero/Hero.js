14c0b48896e0485ebc0d9c6fb281c5bbda97d239
roderknight <dealarcmarcel99@gmail.com>
2021-11-21 11:01:29 -0400

    improvement of redirection on last pages

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,24 +1,60 @@
 import React from 'react'
-import { useParams, Navigate } from 'react-router-dom'
+import { useParams, Navigate, useNavigate } from 'react-router-dom'
 import { GetHeroById } from '../../selectors/getHeroById';
 
 export const Hero = () => {
 
-    const { id } = useParams();
+    const navigate = useNavigate();
 
+    const handleReturn = (publisher) => {
+        navigate( -1 );
+    }
+
+    const { id } = useParams();
     const hero = GetHeroById( id );
+
+    const {
+        id: heroId,
+        alter_ego, 
+        characters,
+        first_appearance,
+        publisher,
+        superhero
+    } = hero;
 
     if( !hero ){
         return <Navigate to='/'/>
     }
 
-    return (
-        <div>
-            <h1>Hero</h1>
+    const imagePath = `/assets/heroes/${ heroId }.jpg`;
 
-            <p>
-                { hero.superhero }
-            </p>
+    return (
+        <div className="row mt-5">
+            <div className="col-4">
+                <img 
+                    src={ imagePath } 
+                    alt={ superhero }
+                    className="img-thumbnail"
+                />
+            </div>
+
+            <div className="col-8">
+                <h3>{ superhero }</h3>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item"> <b>Alter Ego:</b> { alter_ego }</li>
+                    <li className="list-group-item"> <b>Publisher:</b> { publisher }</li>
+                    <li className="list-group-item"> <b>First Apperarance:</b> { first_appearance }</li>
+                </ul>   
+                <h5 className="h5 mt-3">Characters</h5>
+                <p>{ characters }</p>
+
+                <button
+                    className="btn btn-outline-info"
+                    onClick={ () => handleReturn( publisher ) }
+                >
+                    Go back
+                </button>
+            </div>
         </div>
     )
 }
