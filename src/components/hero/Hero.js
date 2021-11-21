3320b7601a8547c373b109f97702c677f094fb7d
roderknight <dealarcmarcel99@gmail.com>
2021-11-21 12:53:25 -0400

    applying effects

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,6 +1,9 @@
-import React, { useMemo } from 'react'
-import { useParams, Navigate, useNavigate } from 'react-router-dom'
+import React, { useMemo } from 'react';
+
+import { useParams, Navigate, useNavigate } from 'react-router-dom';
 import { GetHeroById } from '../../selectors/getHeroById';
+
+import 'animate.css';
 
 export const Hero = () => {
 
@@ -36,11 +39,11 @@ export const Hero = () => {
                 <img 
                     src={ imagePath } 
                     alt={ superhero }
-                    className="img-thumbnail"
+                    className="img-thumbnail animate__animated animate__fadeInLeft"
                 />
             </div>
 
-            <div className="col-8">
+            <div className="col-8 animate__animated animate__fadeIn">
                 <h3>{ superhero }</h3>
                 <ul className="list-group list-group-flush">
                     <li className="list-group-item"> <b>Alter Ego:</b> { alter_ego }</li>
--- src/selectors/getHeroByPublisher.js
+++ src/selectors/getHeroByPublisher.js
@@ -1,7 +1,6 @@
 import { heroes } from "../data/heroes"
 
 export const getHeroByPublisher = ( publisher ) => {
-    console.log('me volvi a renderizar :(');
     const validPublishers = [ 'DC Comics', 'Marvel Comics' ];
     if( !validPublishers.includes( publisher ) ){
         throw new Error(`${ publisher } is not a valid publisher`);
