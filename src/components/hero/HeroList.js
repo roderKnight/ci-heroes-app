cf45638223901176d9def5ad2f5e93b85eb3d81e
roderknight <dealarcmarcel99@gmail.com>
2021-11-20 20:25:50 -0400

    creating HeroCard component and applying better styles

--- src/components/hero/HeroList.js
+++ src/components/hero/HeroList.js
@@ -1,21 +1,21 @@
 import React from 'react'
 import { getHeroByPublisher } from '../../selectors/getHeroByPublisher'
+import { HeroCard } from './HeroCard';
 
 export const HeroList = ({ publisher }) => {
 
     const heores = getHeroByPublisher( publisher );
 
     return (
-        <>
-            <ul>
-                {
-                    heores.map( hero => (
-                        <li key={ hero.id }>
-                            { hero.superhero }
-                        </li>
-                    ) )
-                }
-            </ul>
-        </>
+        <div className="row rows-cols-1 row-cols-md-3 g-3">
+            {
+                heores.map( hero => (
+                    <HeroCard 
+                        key={ hero.id }
+                        { ...hero }    
+                    />
+                ) )
+            }
+        </div>
     )
 }
