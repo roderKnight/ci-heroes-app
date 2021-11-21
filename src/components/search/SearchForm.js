abbbd55d7ad475340b881b950da9572db93d0885
roderknight <dealarcmarcel99@gmail.com>
2021-11-21 14:33:20 -0400

    implementing useForm hook

--- src/components/search/SearchForm.js
+++ src/components/search/SearchForm.js
@@ -1,22 +1,40 @@
 import React from 'react';
 
+import { useForm } from './../hooks/useForm';
 import './../../index.css';
 
 export const SearchForm = () => {
 
 
-    
+    const [ formValues, handleInputChange ] = useForm({
+        searchText: ''
+    })
+
+    const { searchText } = formValues;
+
+    const handleSearch = (e) => {
+        console.log(searchText);
+        e.preventDefault();
+    }
 
     return (
-    <form className="d-flex mx-5">
+    <form className="d-flex mx-5" onSubmit={ handleSearch }>
         <input 
             className="searc-navbar form-control me-2" 
             type="search" 
             placeholder="Search" 
             aria-label="Search" 
-            autoComplete="off"    
+            autoComplete="off"
+            name ="searchText"
+            value={ searchText }  
+            onChange={ handleInputChange }  
         />
-        <button className="btn btn-outline-primary" type="submit">Search</button>
+        <button 
+            className="btn btn-outline-primary" 
+            type="submit"
+        >
+            Search
+        </button>
      </form>
     )
 }
